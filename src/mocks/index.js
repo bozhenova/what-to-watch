e1ef1f76831ce7a69816fcfa43e8fe0d37b540ae
Margo <margo.bozhenova@gmail.com>
2020-07-24 01:43:03 +0300

    Add mock data

---
+++ src/mocks/index.js
@@ -0,0 +1,170 @@
+export const movies = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Comedy`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    image: `img/bohemian-rhapsody.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Crime`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `Macbeth`,
+    image: `img/macbeth.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Documentary`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `Aviator`,
+    image: `img/aviator.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Drama`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `We need to talk about Kevin`,
+    image: `img/we-need-to-talk-about-kevin.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Horror`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `What We Do in the Shadows`,
+    image: `img/what-we-do-in-the-shadows.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Kids & Family`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `Revenant`,
+    image: `img/revenant.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Romance`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  },
+  {
+    title: `Johnny English`,
+    image: `img/johnny-english.jpg`,
+    background: `img/bg-the-grand-budapest-hotel.jpg`,
+    genre: `Sci-Fi`,
+    release: 2014,
+    runtime: 99,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/4/41/Big_Buck_Bunny_medium.ogv`,
+    rating: {
+      score: 8.9,
+      level: `Very good`,
+      count: 240
+    },
+    description: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege. Gustave prides himself on providing first-class service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    crew: {
+      director: `Wes Anderson`,
+      starring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe, Saoirse Ronan, Tony Revoloru, Tilda Swinton, Tom Wilkinson, Owen Wilkinson, Adrien Brody, Ralph Fiennes, Jeff Goldblum`
+    }
+  }
+];
