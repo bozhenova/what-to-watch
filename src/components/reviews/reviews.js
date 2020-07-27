47db619a3e2e6b4155728ccd174654f4ec67e94a
Margo <margo.bozhenova@gmail.com>
2020-07-28 01:26:52 +0300

    Add Reviews component

---
+++ src/components/reviews/reviews.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { parseDate } from '../../utils';
+
+const Reviews = ({ reviews }) => {
+  return (
+    <div className='movie-card__reviews movie-card__row'>
+      <div className='movie-card__reviews-col'>
+        {reviews.map(review => {
+          return (
+            <div className='review'>
+              <blockquote className='review__quote'>
+                <p className='review__text'>{review.comment}</p>
+                <footer className='review__details'>
+                  <cite className='review__author'>{review.user.name}</cite>
+                  <time className='review__date' dateTime={review.date}>
+                    {parseDate(review.date)}
+                  </time>
+                </footer>
+              </blockquote>
+              <div className='review__rating'>{review.rating}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
