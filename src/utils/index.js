87f770f9492568e80719a9f37ce03a79e13fdd2a
Margo <margo.bozhenova@gmail.com>
2020-07-28 01:28:10 +0300

    Add parseRuntime and getRatingLevel functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,35 @@
+import { RatingLevels, Ratings } from '../constants';
+
 export const parseDate = date => {
-  const options = { month: 'long', year: 'numeric' };
+  const options = { month: 'long', year: 'numeric', day: 'numeric' };
   return new Date(date).toLocaleDateString('en-US', options);
+};
+
+export const parseRuntime = time => {
+  const hours = Math.floor(time / 60);
+  const minutes = Math.floor(time % 60);
+  return `${hours >= 1 ? hours + `h` : ``} ${minutes}m`;
+};
+
+export const getRatingLevel = ratingScore => {
+  if (ratingScore >= RatingLevels.BAD && ratingScore < RatingLevels.NORMAL) {
+    return Ratings.BAD;
+  } else if (
+    ratingScore >= RatingLevels.NORMAL &&
+    ratingScore < RatingLevels.GOOD
+  ) {
+    return Ratings.NORMAL;
+  } else if (
+    ratingScore >= RatingLevels.GOOD &&
+    ratingScore < RatingLevels.VERY_GOOD
+  ) {
+    return Ratings.GOOD;
+  } else if (
+    ratingScore >= RatingLevels.VERY_GOOD &&
+    ratingScore < RatingLevels.AWESOME
+  ) {
+    return Ratings.VERY_GOOD;
+  } else {
+    return Ratings.AWESOME;
+  }
 };
