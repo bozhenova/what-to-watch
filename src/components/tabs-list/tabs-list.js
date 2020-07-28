7424e42025df8c85ac869ea931db61ca78c58009
Margo <margo.bozhenova@gmail.com>
2020-07-29 02:05:24 +0300

    Add TabsList component

---
+++ src/components/tabs-list/tabs-list.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Tabs } from '../../constants';
+const shortid = require('shortid');
+
+const TabsList = ({ onTabChange, currentTab }) => {
+  const handleClick = e => {
+    onTabChange(e.target.dataset.tab);
+  };
+
+  return (
+    <nav className='movie-nav movie-card__nav'>
+      <ul className='movie-nav__list'>
+        {Tabs.map(tab => {
+          return (
+            <li
+              key={shortid.generate()}
+              className={`movie-nav__item ${
+                currentTab === tab ? `movie-nav__item--active` : ``
+              }`}
+              onClick={handleClick}
+            >
+              <a className='movie-nav__link' data-tab={tab}>
+                {tab}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default TabsList;
