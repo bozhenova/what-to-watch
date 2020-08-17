7d79de53aafc1670c0ca040afc6f81589cc3cb7b
Margo <margo.bozhenova@gmail.com>
2020-08-18 02:05:07 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require(`path`);
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
 
 module.exports = {
   entry: `./src/index.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
+  },
+  optimization: {
+    splitChunks: {
+      minSize: 250000,
+      maxSize: 300000
+    }
   },
   devServer: {
     contentBase: path.join(__dirname, `public`),
@@ -20,10 +32,36 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: `babel-loader`
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.(css|s[ac]ss)$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
       }
     ]
   },
-  devtool: `source-map`
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './style.[hash].css'
+    }),
+    new OptimizeCssAssetsPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: isProd
+      },
+      chunks: ['main']
+    })
+  ]
 };
