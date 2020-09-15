7cb08a368517465d0d385d389c8f19fd376cd70b
Margo <margo.bozhenova@gmail.com>
2020-09-15 22:34:57 +0300

    Add tests for AddReview, GenresList

--- src/components/add-review/add-review.test.js
+++ src/components/add-review/add-review.test.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { Router } from 'react-router-dom';
 
+import history from '../../history';
+import AddReview from './add-review';
 import rootReducer from '../../redux/reducer';
-import GenresList from './genres-list.js';
 
-describe('Genres List component renders correctly', () => {
-  it('should render a genres list', () => {
-    const genres = [];
-    const onGenreChange = jest.fn();
+describe('Add Review component renders correctly', () => {
+  it('should render Add Review', () => {
     const store = createStore(rootReducer);
     const tree = renderer
       .create(
         <Provider store={store}>
-          <GenresList
-            genres={genres}
-            currentGenre='All genres'
-            onGenreChange={onGenreChange}
-          />
+          <Router history={history}>
+            <AddReview />
+          </Router>
         </Provider>
       )
       .toJSON();
